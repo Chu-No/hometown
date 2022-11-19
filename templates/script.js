a4f62ade514499b34ad8551cbe7278ecf1d2db0f
Kidongg <dlrlehd7@daum.net>
2022-11-19 16:07:33 +0900

    header 고정

--- templates/script.js
+++ templates/script.js
@@ -1,32 +1,15 @@
-const left_btn = document.getElementById('left-btn')
-const right_btn = document.getElementById('right-btn')
+const left_btn = document.getElementById('left-btn');
+const right_btn = document.getElementById('right-btn');
 
-const summer = document.getElementById('summer')
-const david = document.getElementById('david')
-const zeo = document.getElementById('zeo')
-const bien = document.getElementById('bien')
-const eidde = document.getElementById('eidde')
-const nicole = document.getElementById('nicole')
+const person_list_1 = document.getElementById('person-list-1');
+const person_list_2 = document.getElementById('person-list-2');
+const person_list_3 = document.getElementById('person-list-3');
 
-const person_list = [summer, david, zeo, bien, eidde, nicole]
 
 left_btn.addEventListener('click', (e) =>{
-    var slide_num = document.getElementById('member_list').className;
-    console.log(slide_num)
-    if(slide_num == "member_list member_list1"){
-        document.getElementById('member_list').className = 'member_list member_list0';
-    }else if(slide_num == "member_list member_list2"){
-        document.getElementById('member_list').className = 'member_list member_list1';
-    }
-
+  console.log('left-btn');
 })
 
 right_btn.addEventListener('click', (e) =>{
-    var slide_num = document.getElementById('member_list').className;
-    console.log(slide_num)
-    if(slide_num == "member_list member_list0"){
-        document.getElementById('member_list').className = 'member_list member_list1';
-    }else if(slide_num == "member_list member_list1"){
-        document.getElementById('member_list').className = 'member_list member_list2';
-    }
+  console.log('right-btn');
 })
